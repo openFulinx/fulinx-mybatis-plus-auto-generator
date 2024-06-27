673ce02108fcc64afc4165e93d18bad7d7884f18
zichen4479 <30694731+zichen4479@users.noreply.github.com>
2024-06-27 19:07:26 +0800

    init: Initial commit

---
+++ src/main/java/com/fulinx/mybatisPlus/auto/generator/FulinxMybatisPlusAutoGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.fulinx.mybatisPlus.auto.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FulinxMybatisPlusAutoGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FulinxMybatisPlusAutoGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fulinx/mybatisPlus/auto/generator/FulinxMybatisPlusAutoGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fulinx.mybatisPlus.auto.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = FulinxMybatisPlusAutoGeneratorApplication.class)
+class FulinxMybatisPlusAutoGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
